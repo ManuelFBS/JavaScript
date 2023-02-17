a23b8411a591f0934bdc651d05859ba9f6942275
ManuelFBS <manuelf.borrego@gmail.com>
2023-02-17 09:20:34 -0400

    Actualizacion del ejercicio sobre map

--- UsodeMapII.js
+++ UsodeMapII.js
@@ -1,7 +1,8 @@
 const productos = [ 
         { id: 'HFgsfYd43gR7pX0t', nombre: 'Camiseta', precio: 500 }, 
         { id: 'Bbw6547mD8hs34', nombre: 'Zapatos', precio: 2000 }, 
-        { id: 'Ko86Gfn7yWQ5P1', nombre: 'Pantalón', precio: 1500 },
+        { id: 'Ko86Gfn7yWQ5P1', nombre: 'Pantalón', precio: 1500 },        
+        { id: 'Ko700KfwWQ5h9w', nombre: 'Camisa', precio: 2500 },
 ];
 
 const productosConDescuento = productos.map(function(producto) {
@@ -14,4 +15,4 @@ const productosConDescuento = productos.map(function(producto) {
 }); 
 
 console.log(productosConDescuento);
-console.log(productos);
+// console.log(productos);
